e9a8cfb0f571f8e412da17392f3f69b9e185e9e7
martinjhnhadley <martinjohnhadley@gmail.com>
2017-03-09 12:39:45 +0000

    Removed dependence on ply and added cheap copy for later use

---
+++ R/plyr_less_mapvalues.R
@@ -0,0 +1,28 @@
+#' plyr_less_mapvalues
+#'
+#' \code{plyr_less_mapvalues} provides mapvalues from plyr without conflicts arising from the rest of the tidyverse
+#'
+#' @note see plyr documentation
+#'
+plyr_less_mapvalues <- function (x, from, to, warn_missing = TRUE)
+{
+  if (length(from) != length(to)) {
+    stop("`from` and `to` vectors are not the same length.")
+  }
+  if (!is.atomic(x)) {
+    stop("`x` must be an atomic vector.")
+  }
+  if (is.factor(x)) {
+    levels(x) <- mapvalues(levels(x), from, to, warn_missing)
+    return(x)
+  }
+  mapidx <- match(x, from)
+  mapidxNA <- is.na(mapidx)
+  from_found <- sort(unique(mapidx))
+  if (warn_missing && length(from_found) != length(from)) {
+    message("The following `from` values were not present in `x`: ",
+            paste(from[!(1:length(from) %in% from_found)], collapse = ", "))
+  }
+  x[!mapidxNA] <- to[mapidx[!mapidxNA]]
+  x
+}
