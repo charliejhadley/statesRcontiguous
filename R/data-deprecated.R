ad49eaf14c4d22aedcabab526feafd99d4b10934
martinjhnhadley <martinjohnhadley@gmail.com>
2018-04-04 18:26:39 +0100

    Reprecated datasets page

---
+++ R/data-deprecated.R
@@ -0,0 +1,17 @@
+#' @title Datasets with deprecated or defunct names
+#'
+#' @description These are datasets that have been renamed but that have been
+#'   temporarily retained for backward compatibility, or that have been renamed
+#'   and the old names removed. Please change your code to use the new names, as
+#'   the deprecated functions will become defunct in future versions.
+#' @docType data
+#' @name data-deprecated
+#' @keywords internal deprecated
+#' @format See the specific object documentation for the renamed objects.
+NULL
+
+#' @rdname data-deprecated
+#' @name shapefile_details
+#' @details \code{shapefile_details} is defunct; please use
+#'   \code{\link[statesRcontiguous]{statesrcontiguous_shapefile_details}} instead.
+NULL
